e35c60aee7c2b9ae24dde855de925f8462540a93
xChoki <javier.quinmal@hotmail.cl>
2024-06-09 21:01:51 -0400

    User account options

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "home#index"
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 end
