0b35477d7bd1a9bf0b0690a294564613e67894be
xChoki <javier.quinmal@hotmail.cl>
2024-06-09 20:01:27 -0400

    Updated controllers

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :set_task, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, only: %i[ new edit update destroy ]
 
   # GET /tasks or /tasks.json
   def index
