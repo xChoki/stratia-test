292a7c9071dbfb310f7466141f3c2efdce27d397
xChoki <javier.quinmal@hotmail.cl>
2024-06-11 15:02:52 -0400

    Revert "Added i18 language localization"
    This reverts commit d24dd7804362b3e46e302ad1fa8e14c0b57b7418.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,25 +1,2 @@
 class ApplicationController < ActionController::Base
-  before_action :set_locale
-
-  def test
-    render plain: 'Test route works!'
-  end
-
-  def set_locale
-    if params[:locale]
-      I18n.locale = params[:locale]
-      session[:locale] = I18n.locale
-    end
-
-    # Check if the current request path is the root path
-    unless request.path == root_path
-      redirect_back(fallback_location: root_path)
-    end
-  end
-
-  private
-
-  def _set_locale
-    I18n.locale = session[:locale] || I18n.default_locale
-  end
 end
